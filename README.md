757166d1fc8c5551bc93fbdbaf5f3080fa210380
Eric Nielsen <eric@amalgamar.com.br>
2020-01-02 23:08:29 -0500

    Add settings and Apple Terminal support
    Give a default terminal title format, so the module works out-of-the-box
    without any required configuration.
    
    Allow configuring the used hooks, and support Apple Terminal.
    Fixes zimfw/environment#2

--- README.md
+++ README.md
@@ -6,10 +6,21 @@ Sets a custom terminal title.
 Settings
 --------
 
-You can set a custom terminal title containing [prompt expansion escape sequences],
-that is redrawn before each prompt. The example below sets the title to show the
-current directory name:
+The default terminal title format is `%n@%m: %~`, which corresponds to
+`username@host: /path/to/working/directory`.
+
+You can set your own format using [prompt expansion escape sequences].
+The example below sets the format to show the working directory name:
 
     zstyle ':zim:termtitle' format '%1~'
 
+For Apple Terminal, the above is not supported, and the working directory escape
+sequence is sent instead.
+
+By default, the terminal title is updated before each prompt. The used [hooks]
+can be customized with the following zstyle:
+
+    zstyle ':zim:termtitle' hooks 'precmd' 'preexec'
+
 [prompt expansion escape sequences]: http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Simple-Prompt-Escapes
+[hooks]: http://zsh.sourceforge.net/Doc/Release/Functions.html#Hook-Functions
--- init.zsh
+++ init.zsh
@@ -1,14 +1,24 @@
 # sets the window title and updates upon directory change
 # more work probably needs to be done here to support multiplexers
-case ${TERM} in
-  xterm*|*rxvt)
-    termtitle_precmd() {
-      local zformat
-      if zstyle -s ':zim:termtitle' format 'zformat'; then
-        print -Pn "\E]0;${zformat}\a"
-      fi
+() {
+  if [[ ${TERM_PROGRAM} == Apple_Terminal && -z ${INSIDE_EMACS} ]]; then
+    termtitle_update() {
+      print -n "\E]7;$(urlencode ${PWD})\a"
     }
-    autoload -Uz add-zsh-hook && add-zsh-hook precmd termtitle_precmd
-    termtitle_precmd  # we execute it once to initialize the window title
-    ;;
-esac
+  else
+    case ${TERM} in
+      xterm*|*rxvt)
+        local termtitle_format
+        zstyle -s ':zim:termtitle' format 'termtitle_format' || termtitle_format='%n@%m: %~'
+        builtin eval "termtitle_update() { print -Pn '\E]0;${termtitle_format}\a' }"
+        ;;
+      *) return 1 ;;
+    esac
+  fi
+
+  local zhooks zhook
+  zstyle -a ':zim:termtitle' hooks 'zhooks' || zhooks=(precmd)
+  autoload -Uz add-zsh-hook
+  for zhook (${zhooks}) add-zsh-hook ${zhook} termtitle_update
+  termtitle_update  # we execute it once to initialize the window title
+}
