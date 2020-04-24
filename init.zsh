2006bd4bac821f783660172b6c4f5c0966b82b4e
Eric Nielsen <eric@amalgamar.com.br>
2020-04-24 15:48:03 -0500

    Add support to screen
    and use previous escape sequence as default.
    There are way too many TERM variants to worry about. See
    https://wiki.archlinux.org/index.php/Alacritty#%22user@host:cwd%22_in_Window_Title_Bar

--- init.zsh
+++ init.zsh
@@ -1,17 +1,18 @@
-# more work probably needs to be done here to support multiplexers
 () {
   if [[ ${TERM_PROGRAM} == Apple_Terminal ]]; then
     termtitle_update() {
       print -n "\E]7;${PWD}\a"
     }
   else
+    local termtitle_format
+    zstyle -s ':zim:termtitle' format 'termtitle_format' || termtitle_format='%n@%m: %~'
     case ${TERM} in
-      xterm*|rxvt*|alacritty)
-        local termtitle_format
-        zstyle -s ':zim:termtitle' format 'termtitle_format' || termtitle_format='%n@%m: %~'
+      screen)
+        builtin eval "termtitle_update() { print -Pn '\Ek${termtitle_format}\E\\' }"
+        ;;
+      *)
         builtin eval "termtitle_update() { print -Pn '\E]0;${termtitle_format}\a' }"
         ;;
-      *) return 1 ;;
     esac
   fi
 
